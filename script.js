e4fcefcc0fcacac43f779145f196b8fd63ea0797
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-10 22:14:16 +0530

    javascript start of the course

---
+++ script.js
@@ -0,0 +1,11 @@
+//values and variables
+let firstName = "Mathew";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//variable naming convention
+let jonas_singh = "hhgjh"
+
+///Data Types
