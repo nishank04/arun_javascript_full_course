d8112575904b5ad61293790ddcf869a8af8dfab2
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-12 16:28:57 +0530

    Fundamentals

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
 //values and variables
-let firstName = "Mathew";
+//let firstName = "Mathew";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 //variable naming convention
 let jonas_singh = "hhgjh"
 
 ///Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+console.log(typeof true)
+console.log(typeof 23)
+console.log(typeof 'Jonas')
+console.log(typeof javascriptIsFun)
+javascriptIsFun = 'Yes!'
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991;
+console.log(typeof year)
+
+console.log(typeof null)
+
+////let const and var
+let age = 30;
+age = '32';
+console.log(typeof age)
+console.log(age)
+
+const birthYear = 1989;
+// birthYear = 1990;
+
+console.log(birthYear)
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job)
+
+// lastName = 'Yadav';
+// lastName= 'Mohta'
+// console.log(lastName)
+
+///Basic Operators
+//Math Operators
+const now = 2037;
+const ageNishank = now - 1995;
+const ageJonas = now - 1991;
+console.log('ageJonas' + ageJonas, ageNishank)
+
+console.log(ageJonas * 2, ageNishank / 10, 2 ** 3)
+
+const firstName = 'Nishank'
+const lastName = 'Yadav'
+console.log(lastName + ' ' + firstName);
+
+//Asignment Operator
+let x = 10 + 5; // 15
+x += 10; //x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;// x = x + 1
+x--;
+console.log(x)
+
+//Comparison Operators
