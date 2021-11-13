7cd4d2b00c7c9638a0ea54e0f67f42bab7c3f1c4
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-13 19:06:09 +0530

    Basics fundamentals

--- script.js
+++ script.js
@@ -10,63 +10,134 @@ let jonas_singh = "hhgjh"
 
 ///Data Types
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun)
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
 
-console.log(typeof true)
-console.log(typeof 23)
-console.log(typeof 'Jonas')
-console.log(typeof javascriptIsFun)
-javascriptIsFun = 'Yes!'
-console.log(typeof javascriptIsFun)
+// console.log(typeof true)
+// console.log(typeof 23)
+// console.log(typeof 'Jonas')
+// console.log(typeof javascriptIsFun)
+// javascriptIsFun = 'Yes!'
+// console.log(typeof javascriptIsFun)
 
-let year;
-console.log(year)
-console.log(typeof year)
+// let year;
+// console.log(year)
+// console.log(typeof year)
 
-year = 1991;
-console.log(typeof year)
+// year = 1991;
+// console.log(typeof year)
 
-console.log(typeof null)
+// console.log(typeof null)
 
-////let const and var
-let age = 30;
-age = '32';
-console.log(typeof age)
-console.log(age)
+// ////let const and var
+// let age = 30;
+// age = '32';
+// console.log(typeof age)
+// console.log(age)
 
-const birthYear = 1989;
-// birthYear = 1990;
+// const birthYear = 1989;
+// // birthYear = 1990;
 
-console.log(birthYear)
+// console.log(birthYear)
 
-var job = 'programmer';
-job = 'teacher';
-console.log(job)
+// var job = 'programmer';
+// job = 'teacher';
+// console.log(job)
 
-// lastName = 'Yadav';
-// lastName= 'Mohta'
-// console.log(lastName)
+// // lastName = 'Yadav';
+// // lastName= 'Mohta'
+// // console.log(lastName)
 
-///Basic Operators
-//Math Operators
+// ///Basic Operators
+// //Math Operators
+// const now = 2037;
+// const ageNishank = now - 1995;
+// const ageJonas = now - 1991;
+// console.log('ageJonas' + ageJonas, ageNishank)
+
+// console.log(ageJonas * 2, ageNishank / 10, 2 ** 3)
+
+// const firstName = 'Nishank'
+// const lastName = 'Yadav'
+// console.log(lastName + ' ' + firstName);
+
+// //Asignment Operator
+// let x = 10 + 5; // 15
+// x += 10; //x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++;// x = x + 1
+// x--;
+// console.log(x)
+
+// //Comparison Operators
+// console.log(ageJonas > ageNishank) //>, <, >=, <=
+// console.log(ageJonas >= 18)
+
+// const isFullAge = ageJonas >=18
+
 const now = 2037;
-const ageNishank = now - 1995;
 const ageJonas = now - 1991;
-console.log('ageJonas' + ageJonas, ageNishank)
+const ageNishank = now - 2018;
 
-console.log(ageJonas * 2, ageNishank / 10, 2 ** 3)
+console.log(now - 1991 > now - 2018);
 
-const firstName = 'Nishank'
-const lastName = 'Yadav'
-console.log(lastName + ' ' + firstName);
+let x, y;
+x = y = 23 -10 -5; // x = y = 8, x = 8
+console.log(x, y)
 
-//Asignment Operator
-let x = 10 + 5; // 15
-x += 10; //x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++;// x = x + 1
-x--;
-console.log(x)
+const averageAge = (ageJonas + ageNishank) / 2;
+console.log(ageJonas, ageNishank, averageAge)
 
-//Comparison Operators
+////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+/*
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);*/
+
+
+///Strings and Template Literals
+const firstName = 'Nishakn';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const nishank = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!'; //String literal
+console.log(nishank)
+
+const nishankNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}` ///Template literal no need of quotes and no need + sign to concatenate. To concatenate we use ${}
+console.log(nishankNew)
+
+console.log('String with \n\
+multiple \n\
+lines')
+
+console.log(`String with 
+multiple 
+lines`)
