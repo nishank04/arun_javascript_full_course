92eb708a6ff16197e3a647678c2bce6973628151
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-29 21:41:19 +0530

    Intorduction to Objects and a coding challenge

--- 02-FundamentalPart2/script.js
+++ 02-FundamentalPart2/script.js
@@ -200,8 +200,70 @@ if (years.some(v => str.includes('ger') )){
     console.log(`its present`)
 }
 
+///Introduction to Objects
+const nishankArray = [
+    'nishank', 
+    'yadav', 
+    2037 - 1989, 
+    'developer', 
+    ['Saurabh', 'Runish', 'Prateek']
+]
+
+const nishankObject = {
+    firstName: 'nishank',
+    lastName: 'yadav',
+    age: 2037 - 1989,
+    job: 'developer',
+    friends: ['Saurabh', 'Runish', 'Prateek']
+}
+
+console.log(nishankObject)
+
+console.log(nishankObject.lastName)
+
+// const interestedIn = prompt('What do you want to know about nishank? choose between firstname, lastname, age, job and friends')
+// if (nishankObject[interestedIn]) {
+//     console.log(nishankObject[interestedIn])
+// } else {
+//     console.log('wrong request! choose between firstname, lastname, age, job and friends')
+// }
 
 
+// nishankObject.location = 'Delhi';
+// nishankObject['linkedIn'] = 'y.nishank'
+// console.log(nishankObject)
+
+//"nishank ha three friend, and his best friend called Saurabh"
+console.log(`${nishankObject.firstName} has ${nishankObject.friends.length} friends, and his best friend is ${nishankObject.friends[0]}`)
+
+///Object Methods
+const nishankObject1 = {
+    firstName: 'nishank',
+    lastName: 'yadav',
+    birthYear: 1989,
+    job: 'developer',
+    friends: ['Saurabh', 'Runish', 'Prateek'],
+    hasDriverLiscense: false,
+    calcAgeObject: function() {
+        return 2021 - this.birthYear
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAgeObject()} years old and he has ${this.hasDriverLiscense ? 'a' : 'no'} driver's license`
+    }
+}
+
+console.log(nishankObject1.calcAgeObject())
+
+console.log(nishankObject1.getSummary())
 
 
+///Coding Challenge 3
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+// 1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+// 3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+// TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 
