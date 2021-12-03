f78f808dfcce68f86d7fd26401899efab8807cc9
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-12-03 15:54:24 +0530

    for Loops

--- 02-FundamentalPart2/script.js
+++ 02-FundamentalPart2/script.js
@@ -267,3 +267,90 @@ console.log(nishankObject1.getSummary())
 
 // TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 
+
+/*
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}*/
+
+
+//Iteration: The for loop
+
+// console.log('Lifting weights repetetion 1')
+// console.log('Lifting weights repetetion 2')
+// console.log('Lifting weights repetetion 3')
+
+//for loop keeps running while the condition is TRUE
+for (let rep = 1; rep <=30; rep+=3) {
+    console.log(`Lifting weights repetition ${rep}`)
+}
+
+//Looping arrays, Breaking and Continue
+const nishank2 = ['nishank',
+ 'yadav', 
+ 2021 - 1989, 
+ 'developer', 
+ ['raj', 'gaurav', 'sahil'], 
+ true];
+const types = []
+ for (let i = 0; i < nishank2.length; i++) {
+     console.log(nishank2[i], typeof nishank2[i])
+//filling types array
+     types.push(typeof nishank2[i])
+ }
+
+ console.log(types)
+
+ const year1 = [1965,1989, 1995, 1996, 2020, 2021]
+ const ages3 = []
+
+ for (let i = 0; i < year1.length; i++) {
+     ages3.push(2021 - year1[i])
+ }
+console.log(ages3)
+
+//continue and break
+for(let i = 0; i < nishank2.length; i++) {
+    if (typeof nishank2[i] !== 'string') continue;
+
+    console.log(nishank2[i], typeof nishank2[i])
+}
+
+for(let i = 0; i < nishank2.length; i++) {
+    if (typeof nishank2[i] === 'number') break;
+
+    console.log(nishank2[i], typeof nishank2[i])
+}
+///looping backwards
+for (let i = nishank2.length - 1; i>=0; i--) {
+    console.log(nishank2[i])
+}
