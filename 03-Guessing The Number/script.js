d5df6e391396274b9dbc76952ff88e021f14bb10
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-12-14 21:56:05 +0530

    Guess the number game completed

--- 03-Guessing The Number/script.js
+++ 03-Guessing The Number/script.js
@@ -13,4 +13,50 @@ let highScore = 0;
 
 const displayMessage = function (message) {
     document.querySelector('.message').textContent = message
-}
+};
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    //When there is no input
+    if(!guess) {
+//        document.querySelector('.message').textContent = 'No Number';
+        displayMessage('No Number')
+        
+    } //When player wins
+    else if (guess === secretNumber) {
+        displayMessage('Correct Number. Congratulations')
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    }//When guess is wrong
+        else if(guess !== secretNumber) {
+            if (score > 1){
+                displayMessage(guess > secretNumber ? 'Too High' : 'Too Low');
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+        } else {
+            displayMessage('You lost the game');
+        }
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    highScore = 0;
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    displayMessage('Start guessing...')
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
+
